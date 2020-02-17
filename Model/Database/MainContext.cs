bcc7d7b5fa315ef04ab144ef9547bcd90a86ff11
Radmila <radmila@stativa.com.mk>
2020-02-17 23:45:38 +0100

    first commit

---
+++ Model/Database/MainContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Database
+{
+    public class MainContext : DbContext
+    {
+
+        public MainContext(DbContextOptions<MainContext> options)
+           : base(options)
+        { }
+
+        public DbSet<Company> Company { get; set; }
+    }
+}
